fa571fb9d643570a5374134209c89a8d81880c84
Camilo Villegas <ville2007@hotmail.com>
2024-12-15 10:44:28 -0500

    testimonials create

--- js/script.js
+++ js/script.js
@@ -23,6 +23,22 @@ window.onscroll = () => {
 
 
 /*========== swiper ==========*/
+let swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    spaceBetween: 50,
+    loop: true,
+    speed: 500,    
+    spaceBetween: 20,
+    grabCursor: true,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
 
 
 /*========== dark light mode ==========*/
